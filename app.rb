7fd69da0b5c56f8ef4a01e972c0c631fc6454753
Wendy Harris <wendyv510@hotmail.com>
2020-01-28 19:22:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do 
+    enable :sessions
+    set :sessions_secret, "secret"
+  end 
+  
+  get '/' do 
+    
+    erb :index 
+  end 
+  
+  
+  post '/checkout' do
+    @session = session 
+    item = params["item"] 
+    @sessions[:item] = item 
+    
+    erb :view_cart
+  end 
+  
 end
